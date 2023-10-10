64a8f04a85127395af60e5e0c214a3824e213e81
NerdyAlex <rich4alexisu@gmail.com>
2023-10-09 23:50:02 -1200

    init

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
-print("hello world!")
-name = 10
-print(name - 10)
+name = ["anny", "lex", 2, True, 0.5, "mike", 'ixy']
+print(type(name[3]))
+name[0] = "Anny"
+name[1] = "Lex"
+name[6] = 'Ixy'
+name[5] = 'Mike'
+print(name)
+
+
+name1 = list((True, 1.23, 'anny'))
+print(type(name))
--- math_1.py
+++ math_1.py
@@ -1,6 +1,16 @@
-num = 1
-if num >= 1:
-    print("True")
-    
-elif num <= 0:
-    print("False")
+from math import *
+print(abs(-5))
+print(max(40, 50, 100))
+print(min(40, 50, 100))
+print(round(2.49))
+print(bin(500))
+
+
+
+
+sent = input("What is your sentense: ")
+print(sent)
+word1 = input("What word need to change: ")
+word2 = input("with what: ")
+print(sent.replace(word1, word2))
+
